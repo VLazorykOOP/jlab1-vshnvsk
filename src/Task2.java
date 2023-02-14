0dbf7b6d72c8c239e0652e44f52b3b87dd27b767
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2023-02-14 12:36:10 +0200

    update

--- src/Task2.java
+++ src/Task2.java
@@ -10,8 +10,9 @@ public class Task2 {
             n = in.nextInt();
         }
 
-        int[] A;
+        int[] A, B;
         A = new int[n];
+        B = new int[n];
 
         for(int i = 0; i < n; i++){
             System.out.print("A["+i+"]=");
@@ -19,27 +20,38 @@ public class Task2 {
         }
 
         System.out.println("Масив А:");
-        for (int i : A) System.out.print(i + " ");
+        for (int i : A) System.out.print(i + "\t");
         System.out.println();
-
-        ArrayList<Integer> Bb = new ArrayList<>();
-        for(int i = 0; i < A.length; i++){
-            for(int j = i +1; j < A.length; j++){
-                if(A[i] == A[j]){
-                    System.out.println("Елементи, які повторюються:");
-                    System.out.print(A[i]);
-                    System.out.println();
-
-                    System.out.println("Масив В:");
-                    Bb.add(0, A[i]);
-                    System.out.print(Bb);
-                    System.out.println();
+        System.out.println("Array of repeats: ");
+        for (int i = 0; i < A.length ; i++) {
+            boolean repeat = false;
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] == A[j]){
+                    repeat = true;
                 }
             }
+            if(repeat == true) {
+                B[i] = A[i];
+                System.out.print(B[i] + "\t");
+            }
         }
-        //System.out.println("Масив B:");
-        //System.arraycopy(A, 0, B, 0, n);
-        //for (int i : B) System.out.print(i + " ");
+
+       // ArrayList<Integer> Bb = new ArrayList<>();
+//        for(int i = 0; i < A.length; i++){
+//            for(int j = i + 1; j < A.length; j++){
+//                if(A[i] == A[j]){
+//                    System.out.println("Елементи, які повторюються:");
+//                    System.out.print(A[i]);
+//                    System.out.println();
+//
+//                    System.out.println("Масив В:");
+//
+//                    Bb.add(0, A[i]);
+//                    System.out.print(Bb);
+//                    System.out.println();
+//                }
+//            }
+//        }
     }
 }
 
